6d3eeae46ad53e1d7b69e72e071647c8a019ad4c
suhaibsuhai <suhaibhamza300@gmail.com>
2024-03-25 00:05:27 +0000

    create index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('./db.js');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.set('strictQuery', true);
+
+mongoose.connect('mongodb://localhost:27017/blogApp', {
+    family: 4,
+})
+.then(() => {
+    console.log('Connection to MongoDB successful');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+
 const app = express();
 const route = require('./routes.js');
 
